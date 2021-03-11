e36ce74d9d87d3ad40665eee599b1ccd461c9788
adiheric <hadiheric@gmail.com>
2021-03-11 08:39:50 +0100

    sum of two numbers

---
+++ ex08/src/index.js
@@ -0,0 +1,10 @@
+function main(numOne, numTwo) {
+
+    var numOne = 15;
+    var numTwo = 8;
+    var sum = numOne + numTwo;
+   return sum; 
+}
+
+console.log(main(numOne, numTwo));
+module.exports = main;
