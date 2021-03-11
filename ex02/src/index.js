96d535d3ef657676ec2318d9452f5d5073275ee5
adiheric <hadiheric@gmail.com>
2021-03-11 06:43:04 +0100

    komentari

---
+++ ex02/src/index.js
@@ -0,0 +1,18 @@
+function main() {
+
+    // This is a single line JavaScript comment
+
+/* document.write("This is a multi-line comment!");
+document.write("This is a multi-line comment!");
+document.write("This is a multi-line comment!");
+document.write("This is a multi-line comment!");
+document.write("This is a multi-line comment!");
+document.write("This is a multi-line comment!");
+*/
+
+
+}
+
+console.log(main());
+
+module.exports = main;
